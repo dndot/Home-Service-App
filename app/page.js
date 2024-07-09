318ba6137b9bbf880e619f6b57a602d2c29b41e8
Dhiraj Neupane <d.neupane077@gmail.com>
2024-07-09 11:02:19 +0545

    setup hygraph and get category list

--- app/page.js
+++ app/page.js
@@ -1,12 +1,28 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./_components/Hero";
-
+import CategoryList from "./_components/CategoryList";
+import GlobalApi from "./_services/GlobalApi";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory()
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <div>
-      <Hero/>
+      <Hero />
+
+      <CategoryList />
     </div>
   );
 }
